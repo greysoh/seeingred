f64152334072a4b5f0d90e3c90c2dfe31be5f827
greysoh <greysoh@pm.me>
2023-10-19 14:49:09 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# SeeingRed
+A stupidly simple YouTube adblock implementation.
+## Why?
+uBlock Origin was starting to fail on me for YouTube, and I was too lazy to do their method to fix it.
+## How?
+Remember seeing [stuff like this?](https://www.youtube.com/watch?v=HngsmO2ECIs). This implements that in JavaScript by skipping ahead 1second, 10ms at a time.
---
+++ src/index.mjs
@@ -0,0 +1,49 @@
+console.log("[debug] Monitoring begin.");
+let oldHref = window.location.href;
+let isRunningAdObserver = window.location.href.includes("watch?v=");
+
+const observer = new MutationObserver((mutations) => {
+  if (oldHref != window.location.href) {
+    oldHref = window.location.href;
+    isRunningAdObserver = window.location.href.includes("watch?v=");
+  }
+});
+
+async function doBackgroundAdsChecker() {
+  while (true) {
+    if (isRunningAdObserver) {      
+      const usualAdTextElement = document.getElementsByClassName("ytp-title-link yt-uix-sessionlink ytp-title-fullerscreen-link")[0];
+      const usualTitleElement = document.querySelector("yt-formatted-string.style-scope.ytd-watch-metadata");
+      
+      if (!usualAdTextElement || !usualTitleElement) {
+        await new Promise((i) => setTimeout(i, 100));
+        continue;
+      }
+  
+      const videoPlayer = document.querySelector("video.video-stream.html5-main-video");
+  
+      while (usualAdTextElement.innerText != usualTitleElement.innerText) {
+        // Psychic attack this mofo
+        videoPlayer.muted = true;
+        videoPlayer.pause();
+        videoPlayer.currentTime += 1;
+        videoPlayer.play();
+  
+        await new Promise((i) => setTimeout(i, 10));
+      }
+
+      videoPlayer.muted = false;
+    }
+  
+    await new Promise((i) => setTimeout(i, 100));
+  }
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  doBackgroundAdsChecker();
+
+  observer.observe(document.querySelector("body"), {
+    childList: true,
+    subtree: true
+  });
+});
